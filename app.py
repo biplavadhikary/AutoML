7280605c285a4322d08646c498df21d061a2c00f
biplavadhikary <42610650+biplavadhikary@users.noreply.github.com>
2020-03-06 21:21:28 +0530

    added plots

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask , render_template, request, url_for, redirect, session
+from flask import Flask , render_template, request, url_for, redirect, session, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from visualizeScript import buildSvg
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
+app.config['DATASET_FOLDER'] = './datasets'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filestorage.db'
 db = SQLAlchemy(app)
+
+@app.route('/datasets/<path:filename>', methods=["GET"])
+def svgRender(filename):
+    print('Called here')
+    return send_from_directory(app.config['DATASET_FOLDER'], filename)
 
 class FileContents(db.Model):
     id = db.Column(db.String(300),primary_key=True)
@@ -14,7 +21,7 @@ class FileContents(db.Model):
     attrib_list = db.Column(db.String(500))
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     completed = db.Column(db.Integer, default=0)
-    #data = db.Column(db.LargeBinary)
+    #data = db.1Column(db.LargeBinary)
 
     def __repr__ (self):
         return f'<Added {self.id}>'
@@ -42,6 +49,7 @@ def handleUpload():
     # extracting the attributes from header and save it to sesssion
     attr = extractAttribList('C:/Users/KIIT/Desktop/AutoML/datasets/'+filenameUn)
     session['dataset'] = attr
+    session['datasetName'] = filenameUn
 
     # save to db for later use
     new_dataset = FileContents(id=filenameUn,name=filenameOr,attrib_list=str(attr))
@@ -62,6 +70,14 @@ def select():
         attrib = ['Upload your dataset first', 'Go Back To Index']
     return render_template('selection.html',attrib=attrib)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/plot',methods=['POST'])
+def plot():
+    if request.method == 'POST':
+        target = request.form.get('target_y')
+        folderName = session['datasetName'].split('.')[0]
+        import visualizeScript as vs
+        plottypes = vs.buildSvg(folderName,target)
+        return render_template('visualize.html',folderName=folderName,plottypes=plottypes)
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
---
+++ visualizeScript.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import os
+from autoviz.AutoViz_Class import AutoViz_Class
+
+def buildSvg(folderName, targetY):
+    Av = AutoViz_Class()
+    print(f'foldername: {folderName},  target={targetY}')
+    df = pd.read_csv(f'datasets/{folderName}.csv')
+    os.mkdir(f'datasets/{folderName}') 
+
+    sep = ' '
+    target = targetY
+
+    dft = Av.AutoViz(
+        '',
+        sep,
+        target,
+        df,
+        header=0,
+        verbose=2,
+        lowess=False,
+        chart_format="svg",
+        max_rows_analyzed=150000,
+        max_cols_analyzed=30,
+    )
+
+    PltType = [Av.scatter_plot, Av.pair_scatter, Av.dist_plot, Av.pivot_plot, Av.violin_plot, Av.heat_map, Av.bar_plot, Av.date_plot]
+
+    for plottypes in PltType:
+        i=0
+        name = plottypes['name']
+        plottypes['loc']=[]
+        for plotlist in plottypes['plots']:
+            j=0
+            for plot in plotlist:
+                f = open(f'./datasets/{folderName}/{name}-{i+1}-{j+1}.svg', 'w', encoding="utf-8")
+                f.write(plot)
+                f.close()
+                plottypes['loc'].append(f'{name}-{i+1}-{j+1}.svg')
+                j+=1
+            print('')
+            i+=1
+    return PltType
+
+def displayPlotData(Av):
+    PltType = [Av.scatter_plot, Av.pair_scatter, Av.dist_plot, Av.pivot_plot, Av.violin_plot, Av.heat_map, Av.bar_plot, Av.date_plot]
+    for plottypes in PltType:
+        for key, value in plottypes.items():
+            if key != 'plots' :
+                print(f'{key}: {value}')
+        print('\n')
