725468ecc2b4ea63568cf32544285db18d1b0619
biplavadhikary <42610650+biplavadhikary@users.noreply.github.com>
2020-05-31 16:44:48 +0530

    updated meta tag and datasets

--- datasets/dee011c3c8857b6f2f9df7e11acb8324_bike-buyer_clf/pipelineClf.py
+++ datasets/dee011c3c8857b6f2f9df7e11acb8324_bike-buyer_clf/pipelineClf.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.9075276855859048
-exported_pipeline = RandomForestClassifier(bootstrap=False, criterion="entropy", max_features=0.7500000000000001, min_samples_leaf=18, min_samples_split=12, n_estimators=100)
+# Average CV score on the training set was: 0.9091359199944125
+exported_pipeline = ExtraTreesClassifier(bootstrap=False, criterion="entropy", max_features=0.8, min_samples_leaf=3, min_samples_split=20, n_estimators=100)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
